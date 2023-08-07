df551b5195aa3fb044ec76272d5c721616df35d1
Jose Buschiazzo <jbuschiazzo@electraba.com.ar>
2023-08-07 12:23:26 -0300

    Contactanos

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
-import Card from './Card'
+import { useState } from 'react';
+import '../styles/contacto.css';
+
 function Contacto() {
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [consulta, setConsulta] = useState("");
+  const [enviado, setEnviado] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // agregar la lógica para enviar el correo 
+    setEnviado(true);
+  }
+
   return (
-    <div>
-            <h1 className='p-5 m-5'> CONTACTO </h1>
-        <Card
-          cardTitle="Base San Martín"
-          cardText="Está localizada en el islote San Martín del grupo de islotes Debenham en Bahía Margarita, al oeste de la Península Antártica, siendo la más occidental de las bases argentinas. " />
-          <Card
-          cardTitle="Base San Martín"
-          cardText="Está localizada en el islote San Martín del grupo de islotes Debenham en Bahía Margarita, al oeste de la Península Antártica, siendo la más occidental de las bases argentinas. " />
-          <Card
-          cardTitle="Base San Martín"
-          cardText="Está localizada en el islote San Martín del grupo de islotes Debenham en Bahía Margarita, al oeste de la Península Antártica, siendo la más occidental de las bases argentinas. " />
+    <div className="contact-form">
+      {enviado ? (
+        <h4>Gracias por contactarnos. Tu consulta ha sido enviada con exito!.</h4>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h2>Contactanos</h2>
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Correo Electrónico"
+            value={correo}
+            onChange={(e) => setCorreo(e.target.value)}
+            required
+          />
+          <textarea
+            placeholder="Escribe tu consulta aquí..."
+            value={consulta}
+            onChange={(e) => setConsulta(e.target.value)}
+            required
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      )}
     </div>
-  )
+  );
 }
 
-export default Contacto
+export default Contacto;
