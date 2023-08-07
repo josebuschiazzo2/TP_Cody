011e7af2241b57fc0665cecc7d366fb028c42e0f
Jose Buschiazzo <jbuschiazzo@electraba.com.ar>
2023-08-07 16:03:13 -0300

    Contactanos

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -9,6 +9,7 @@ import '../styles/noticias.css';
 
 function Noticias() {
   const [newsData, setNewsData] = useState([]);
+  const [newsToShow, setNewsToShow] = useState(21); // Estado para el número de noticias a mostrar
 
   const fetchNews = async () => {
     const apiKey = '8613c9fce4674e0994c38ffa6850524c'; //lo pongo aparte para que no se me pierda y no se donde lo deje en el mail ja
@@ -17,22 +18,28 @@ function Noticias() {
     try {
       const response = await axios.get(apiUrl);
       const sortedNews = response.data.articles
-        .slice(0, 21)
+        .filter((item) => item.urlToImage) //esto me faltaba
+        .slice(0,newsToShow)
         .sort((a, b) => parseISO(b.publishedAt).getTime() - parseISO(a.publishedAt).getTime());
       setNewsData(sortedNews);
     } catch (error) {
       console.error('Error fetching news:', error);
-    }
+      }
   };
 
-  useEffect(() => {fetchNews();}, []);
+  useEffect(() => {
+    fetchNews();
+  }, [newsToShow]); // Ejecuta fetchNews() cada vez que newsToShow cambia
+
+  const handleShowMoreNews = () => {
+    setNewsToShow((prevNewsToShow) => prevNewsToShow + 21); // Incrementa el número de noticias a mostrar en 21
+  };
 
   return (
     <div id="Noticias">
       <Navbar />
       <div className="containernews">
         <h1 className='p-5 m-5'> NOTICIAS</h1>
-
         <div className="row">
           {newsData.map((item, index) => (
             <div key={index} className="col-lg-4 col-md-6 col-sm-12">
@@ -44,6 +51,9 @@ function Noticias() {
               />
             </div>
           ))}
+        </div>
+        <div className="load-more-news">
+          <button onClick={handleShowMoreNews}>+</button>
         </div>
       </div>
       <Footer />
