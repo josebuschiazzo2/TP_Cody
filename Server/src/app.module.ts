a61123940c2d2e52d006619396f15a97bcf22ee0
EvelinVidal <evelinvidal@tdf.edu.ar>
2023-10-21 21:20:28 -0300

    password app module

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -14,7 +14,7 @@ import { CommentModule } from './comment/comment.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'Misql8',
+      password: '',
       database: 'cody',
       entities: [__dirname + '/**/**/**.entity{.ts,.js}'],
       synchronize: true,
