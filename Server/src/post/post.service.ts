d42606bda00246702a7c7b22345f6851ff987a61
EvelinVidal <evelinvidal@tdf.edu.ar>
2023-10-25 14:35:07 -0300

    comunidad

--- Server/src/post/post.service.ts
+++ Server/src/post/post.service.ts
@@ -10,46 +10,45 @@ export class PostService {
   constructor(
     @InjectRepository(Post)
     public postRepository: Repository<Post>,
-    
   ) {}
-//*****     CREATE NEW POST     ***** */
-async create(postDto: PostDto, userID: User, username: string) { 
-  if (postDto.post !== "") {
-    const publicacion: Post = await this.postRepository.save(new Post(postDto.post, userID, username));
-    if (!publicacion) {
-      return "Error al cargar la publicación";
+  //*****     CREATE NEW POST     ***** */
+  async create(postDto: PostDto, userID: User, username: string) {
+    if (postDto.post !== '') {
+      const publicacion: Post = await this.postRepository.save(
+        new Post(postDto.post, userID, username),
+      );
+      if (!publicacion) {
+        return 'Error al cargar la publicación';
+      }
+      return `La publicación se creó correctamente, usuario: ${username}`;
+    } else {
+      return 'Publicación vacía';
     }
-    return `La publicación se creó correctamente, usuario: ${username}`;
-  } else {
-    return "Publicación vacía";
   }
-}
 
-  
-//*****     READ POST     ***** */
+  //*****     READ POST     ***** */
   async findAll() {
-    return await this.postRepository.find()
+    return await this.postRepository.find();
   }
 
-
-//*****     DELETE POST     ***** */
-async remove(id: number):Promise<any> {
-  try {
-    const aux: FindOneOptions = { where: { id: id } };
-    let post: Post = await this.postRepository.findOne(aux);
-    if (!post) throw new Error('no se encontró publicacion');
-    else {
-      await this.postRepository.remove(post);
-      return { id: id, message: 'se elimino correctamente' }; // -> retorna json
+  //*****     DELETE POST     ***** */
+  async remove(id: number): Promise<any> {
+    try {
+      const aux: FindOneOptions = { where: { id: id } };
+      let post: Post = await this.postRepository.findOne(aux);
+      if (!post) throw new Error('no se encontró publicacion');
+      else {
+        await this.postRepository.remove(post);
+        return { id: id, message: 'se elimino correctamente' }; // -> retorna json
+      }
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: ' ' + error,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
-  } catch (error) {
-    throw new HttpException(
-      {
-        status: HttpStatus.NOT_FOUND,
-        error: ' ' + error,
-      },
-      HttpStatus.NOT_FOUND,
-    );
   }
-}
 }
